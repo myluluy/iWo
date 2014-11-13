e8cbc335731f287d93fc06e416dcefc907b62973
Xiaojue <designsor@gmail.com>
2014-11-13 14:06:10 +0800

    add utils

--- src/mods/class.js
+++ src/mods/class.js
@@ -1,3 +1,16 @@
-iwo.define('mods/class',function(require){
-    return 'class';
+/**
+ * @author xiaojue[designsor@gmail.com]
+ * @date 20141113
+ * @fileoverview 可多级继承的class
+ */
+iwo.define('mods/class',['mods/utils'],function(require){
+
+  var utils = require('mods/utils');
+
+  function Class(source){
+
+  }
+
+  return Class;
+
 });
--- src/mods/utils.js
+++ src/mods/utils.js
@@ -1,3 +1,28 @@
-iwo.define('mods/utils',function(require){
-    return 'utils';
+/*
+ * @author xiaojue[designsor@gmail.com]
+ * @date 20141113
+ * @fileoverview base utils
+ */
+iwo.define('mods/utils', function(require) {
+
+  var utils = {
+    extend: function(source, target, preserve) {
+
+    },
+    mixto: function(array, target) {
+
+    },
+    remove: function(source, name, safe) {
+
+    },
+    copy: function(source) {
+
+    },
+    implement: function(array) {
+
+    }
+  };
+
+  return utils;
+
 });
