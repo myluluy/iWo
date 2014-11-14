316ba2ba88e305e4065be3220770ddb5c7360be4
myluluy <myluluy@gmail.com>
2014-11-14 12:01:42 +0800

    增加range的部分基础功能

--- src/mods/range/base.js
+++ src/mods/range/base.js
@@ -1,5 +1,5 @@
 /**
- * @author myluluy@gmail.com
+ * @author arck[myluluy@gmail.com]
  * @date 20141113
  * @fileoverview range  for iwo
  */
@@ -27,21 +27,24 @@ iwo.register('mods/range/base', [], function(require) {
     this.doc = doc || document;
 
     this.collapsed = null;
-  
-  }
 
+  }
 
 
   Range.prototype = {
 
     /*
-     *  constructor
+     * @constructor
      * */
     constructor: Range,
 
-    setSc: function(container, offset) {},
+    setSc: function(container, offset) {
+      updateRange(this, container, offset, true);
+    },
 
-    setEc: function(container, offset) {},
+    setEc: function(container, offset) {
+      updateRange(this, container, offset, false);
+    },
 
     setScAfter: function(container) {},
 
@@ -49,8 +52,60 @@ iwo.register('mods/range/base', [], function(require) {
 
     setScBefore: function(container) {},
 
-    setEcBefore: function(container) {}
+    setEcBefore: function(container) {},
 
+    collapse: function(toStart) {
+      var cont = ['sc', 'ec'],
+        ofst = ['so', eo],
+        t1 = toStart ? 0 : 1,
+        t2 = toStart ? 1 : 0;
+      range[cont[t1]] = range[cont[t2]];
+      range[ofst[t1]] = range[ofst[t2]];
+      this.collapsed = true;
+      return this;
+    }
+
+  }
+
+
+
+  /*prative methods*/
+  /*
+   *  @method : update the range.collapsed;
+   * */
+  function updateCollapse(range) {
+    range.collapsed = range && range.sc && range.ec && range.sc === range.ec && range.so === range.eo;
+    return range;
+  }
+
+  /*
+   * @method :fixRange
+   * fix container & offset to suitable;
+   * */
+  function fixRange(range) {
+    if (!range) {
+      return range;
+    }
+    var sc = range.sc,
+      so = range.so,
+      ec = range.ec,
+      eo = range.eo;
+    collapsed = range.collapsed;
+    return range;
+
+  }
+
+  /*
+   * @method : updateRange
+   * set the container & offset for range;
+   * */
+  function updateRange(range, container, offset, isStart) {
+    var pName = isStart ? 's' : 'e';
+    range[pName + 'c'] = container;
+    range[pName + 'o'] = offset;
+    updateCollapse(range);
+    fixRange(range);
+    return range;
   }
 
   return Range;
