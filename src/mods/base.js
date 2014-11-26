b04adbe5807df0a5e26c230b984fa2e26fdc57b4
Xiaojue <designsor@gmail.com>
2014-11-26 14:42:29 +0800

    fix base

--- src/mods/base.js
+++ src/mods/base.js
@@ -15,26 +15,11 @@ iwo.define('mods/base', ['mods/event', 'mods/class', 'mods/attr', 'mods/utils'],
 
   var Base = new Class({
     initialize: function() {
-      this.set('t', 1, {
-        readOnly: true
-      });
+
     }
   });
 
   Base.extend([Event, Attr]);
-
-  var test = new Base({
-    hoho: {
-      a: 1
-    }
-  });
-
-  test.once('change:hoho',function(){
-    console.log(123); 
-  });
-
-  test.set('hoho',2);
-  test.set('hoho',2);
 
   return Base;
 
