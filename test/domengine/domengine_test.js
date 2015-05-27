ecb3804d64b798306dbbe7ea6242e654f0dba36d
liulyliu <myluluy@gmail.com>
2015-05-27 10:27:44 +0800

    test domengine

--- test/domengine/domengine_test.js
+++ test/domengine/domengine_test.js
@@ -1,4 +1,6 @@
 iwo.define('../test/domengine/domengine_test',['mods/domengine/domengine'],function(require){
     var dom = require('mods/domengine/domengine');
-    console.info(dom);
+
+    var div = dom.createElement('div');
+    console.info(div);
 });
